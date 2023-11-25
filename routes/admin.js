e3f7b5a28604954b4f00afa06ae0a95faf1c68de
phuong090802 <20110547@student.hcmute.edu.vn>
2023-11-25 21:08:51 +0700

    Update

--- routes/admin.js
+++ routes/admin.js
@@ -10,12 +10,14 @@ import { isNotIdAdmin } from '../middlewares/auth.js';
 
 const router = express.Router();
 
-router.route('/users/:username')
-    .patch(isNotIdAdmin, handleUpdatePasswordUserByUserName)
-
 router.route('/users/:id')
     .patch(isNotIdAdmin, handleUpdatePasswordUserById)
     .delete(isNotIdAdmin, handleDeleteUser)
+
+router.route('/users/:username')
+    .patch(isNotIdAdmin, handleUpdatePasswordUserByUserName)
+
+
 
 router.route('/users')
     .post(handleCreateUser)
