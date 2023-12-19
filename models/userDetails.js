a7d3f19b25b56fe229efa87738cad0ceae801b5e
phuong090802 <20110547@student.hcmute.edu.vn>
2023-12-19 23:00:45 +0700

    Update

--- models/userDetails.js
+++ models/userDetails.js
@@ -1,49 +1,53 @@
 import mongoose from 'mongoose';
 import validator from 'validator';
 
-const userDetailsSchema = new mongoose.Schema({
+const userDetailsSchema = new mongoose.Schema(
+  {
     phone: {
-        type: String,
-        required: [true, 'Vui lòng nhập số điện thoại'],
-        unique: true,
-        validate: {
-            validator: function (value) {
-                return validator.isMobilePhone(value, 'vi-VN') || validator.isMobilePhone(value, 'vi-VN', { strictMode: false });
-            },
-            message: 'Vui lòng nhập số điện thoại hợp lệ ở Việt Nam'
-        }
+      type: String,
+      required: [true, 'Vui lòng nhập số điện thoại'],
+      unique: true,
+      validate: {
+        validator: function (value) {
+          return (
+            validator.isMobilePhone(value, 'vi-VN') ||
+            validator.isMobilePhone(value, 'vi-VN', { strictMode: false })
+          );
+        },
+        message: 'Vui lòng nhập số điện thoại hợp lệ ở Việt Nam',
+      },
     },
     email: {
-        type: String,
-        required: [true, 'Vui lòng nhập email'],
-        maxLength: [255, 'Email không được vượt quá 255 ký tự'],
-        unique: true,
-        validate: {
-            validator: function (value) {
-                return validator.isEmail(value);
-            },
-            message: 'Vui lòng nhập email hợp lệ'
-        }
+      type: String,
+      required: [true, 'Vui lòng nhập email'],
+      maxLength: [255, 'Email không được vượt quá 255 ký tự'],
+      unique: true,
+      validate: {
+        validator: function (value) {
+          return validator.isEmail(value);
+        },
+        message: 'Vui lòng nhập email hợp lệ',
+      },
     },
     image: {
-        ref: {
-            type: String
-        },
-        url: {
-            type: String
-        }
+      ref: {
+        type: String,
+      },
+      url: {
+        type: String,
+      },
     },
     degree: {
-        type: String,
-        required: [true, 'Vui lòng nhập học vị'],
+      type: String,
+      required: [true, 'Vui lòng nhập học vị'],
     },
     user: {
-        type: mongoose.Schema.Types.ObjectId,
-        required: [true, 'Thiếu thông tin người dùng'],
-        ref: 'User'
-    }
-
-}, { collection: 'user-details', timestamps: true }
+      type: mongoose.Schema.Types.ObjectId,
+      required: [true, 'Thiếu thông tin người dùng'],
+      ref: 'User',
+    },
+  },
+  { collection: 'user-details', timestamps: true, validateBeforeSave: false }
 );
 
 const UserDetails = mongoose.model('UserDetails', userDetailsSchema);
