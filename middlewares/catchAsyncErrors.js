4169d7c9fda7e7cf72c6e8371b12b5d569b0cc0e
phuong090802 <20110547@student.hcmute.edu.vn>
2023-12-20 17:29:40 +0700

    Update

--- middlewares/catchAsyncErrors.js
+++ middlewares/catchAsyncErrors.js
@@ -1,5 +1,4 @@
 export default function catchAsyncErrors(func) {
-    console.info('Inside catchAsyncErrors');
     return function (req, res, next) {
         return Promise.resolve(func(req, res, next))
             .catch(next);
